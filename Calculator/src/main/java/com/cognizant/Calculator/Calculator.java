31ac59809ad3ee716dee086b1087bd75fa7055bd
Jose Maria <45007510+jmorenete@users.noreply.github.com>
2018-12-19 12:03:01 +0000

    multiplication finished

--- Calculator/src/main/java/com/cognizant/Calculator/Calculator.java
+++ Calculator/src/main/java/com/cognizant/Calculator/Calculator.java
@@ -7,4 +7,8 @@ public class Calculator {
 
 	}
 
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> Add files via upload
--- Calculator/src/main/java/com/cognizant/Calculator/Multiplication.java
+++ Calculator/src/main/java/com/cognizant/Calculator/Multiplication.java
@@ -1,5 +1,15 @@
 package com.cognizant.Calculator;
 
-public class Multiplication {
+public class Multiplication {	
+		int result = 0;
+		public void multiply(int num1,int num2) {
+			result = num1*num2;
+		}
+		
+		public int getResult() {
+			return result;
+		}
+
+
 
 }
