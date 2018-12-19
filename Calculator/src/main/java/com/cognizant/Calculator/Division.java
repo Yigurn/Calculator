89c5ea2a18d026b057746bade5cb3922be62dd0e
Joshua Lewis <45007621+Yigurn@users.noreply.github.com>
2018-12-19 11:44:02 +0000

    Update Division.java

--- Calculator/src/main/java/com/cognizant/Calculator/Division.java
+++ Calculator/src/main/java/com/cognizant/Calculator/Division.java
@@ -2,4 +2,14 @@ package com.cognizant.Calculator;
 
 public class Division {
 
+	int value = 0;
+	
+	public Division(int a, int b) {
+		value = a + b;
+	}
+	
+	public int getResult()
+	{
+		return value;
+	}			
 }
